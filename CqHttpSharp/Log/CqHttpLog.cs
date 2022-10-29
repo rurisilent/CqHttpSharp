4520d5a2c1fa6f567213c9af5e5d327dc9766336
rurisilent <rurisilent@gmail.com>
2022-10-29 22:21:48 +0800

    Add logs

---
+++ CqHttpSharp/Log/CqHttpLog.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CqHttpSharp.Log
+{
+    public enum CqHttpLogType
+    {
+        normal,
+        warning,
+        error
+    }
+
+    public delegate void EvtCqHttpLog(string content, CqHttpLogType type);
+}
