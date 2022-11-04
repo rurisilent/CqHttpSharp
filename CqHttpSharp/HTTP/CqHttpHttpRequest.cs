466955dbf11d87af3a08904a23ef499841c098a6
rurisilent <rurisilent@gmail.com>
2022-11-05 00:52:11 +0800

    Adds a http request with custom headers

--- CqHttpSharp/HTTP/CqHttpHttpRequest.cs
+++ CqHttpSharp/HTTP/CqHttpHttpRequest.cs
@@ -50,6 +50,47 @@ namespace CqHttpSharp.HTTP
             }
         }
 
+        public async Task<T> APIHttpUrlCustomHeaderAsync<T>(string url, string method, string attr, WebHeaderCollection headers)
+        {
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(string.Format(url));
+            request.Headers = headers;
+            request.Method = method;
+            request.Timeout = 1000000;
+            request.ContentType = "application/x-www-form-urlencoded";
+            request.ServerCertificateValidationCallback = (sender, cert, chain, sslPolicyErrors) => { return true; };
+            if (method != "GET")
+            {
+                byte[] data = Encoding.UTF8.GetBytes(attr);
+                request.ContentLength = data.Length;
+                Stream stream = await request.GetRequestStreamAsync();
+                stream.Write(data, 0, data.Length);
+                stream.Close();
+            }
+
+            try
+            {
+                using (HttpWebResponse response = (HttpWebResponse)await request.GetResponseAsync())
+                {
+                    Stream streamRes = response.GetResponseStream();
+                    string result = new StreamReader(streamRes, Encoding.UTF8).ReadToEnd();
+
+                    try
+                    {
+                        return JsonConvert.DeserializeObject<T>(result);
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine($"Error Parsing JSON Respond : {e.Message} \n {e.StackTrace}");
+                        return default(T);
+                    }
+                }
+            }
+            catch
+            {
+                return default(T);
+            }
+        }
+
         public async Task<string> APIHttpUrlAsync(string url, string method, string attr)
         {
             HttpWebRequest request = (HttpWebRequest)WebRequest.Create(string.Format(url));
